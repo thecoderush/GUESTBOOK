7bd032e5a01861d7b04386daf1915be26eb43926
thecoderush <pheanuk@gmail.com>
2022-03-29 14:24:56 +0200

    Add 7.4: Dumping and Restoring Database Data

---
+++ dump.sql
@@ -0,0 +1,78 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.6
+-- Dumped by pg_dump version 14.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: conference; Type: TABLE DATA; Schema: public; Owner: symfony
+--
+
+COPY public.conference (id, city, year, is_international) FROM stdin;
+1	Amsterdam	2022	t
+2	Paris	2022	f
+\.
+
+
+--
+-- Data for Name: comment; Type: TABLE DATA; Schema: public; Owner: symfony
+--
+
+COPY public.comment (id, conference_id, author, text, email, created_at, photo_filename) FROM stdin;
+\.
+
+
+--
+-- Data for Name: doctrine_migration_versions; Type: TABLE DATA; Schema: public; Owner: symfony
+--
+
+COPY public.doctrine_migration_versions (version, executed_at, execution_time) FROM stdin;
+DoctrineMigrations\\Version20220326164202	2022-03-26 16:44:40	66
+\.
+
+
+--
+-- Data for Name: messenger_messages; Type: TABLE DATA; Schema: public; Owner: symfony
+--
+
+COPY public.messenger_messages (id, body, headers, queue_name, created_at, available_at, delivered_at) FROM stdin;
+\.
+
+
+--
+-- Name: comment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: symfony
+--
+
+SELECT pg_catalog.setval('public.comment_id_seq', 3, true);
+
+
+--
+-- Name: conference_id_seq; Type: SEQUENCE SET; Schema: public; Owner: symfony
+--
+
+SELECT pg_catalog.setval('public.conference_id_seq', 2, true);
+
+
+--
+-- Name: messenger_messages_id_seq; Type: SEQUENCE SET; Schema: public; Owner: symfony
+--
+
+SELECT pg_catalog.setval('public.messenger_messages_id_seq', 1, false);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
