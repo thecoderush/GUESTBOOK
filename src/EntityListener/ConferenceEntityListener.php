4243f859dfc2bbd100e3c42c2351229d8e744ae2
thecoderush <pheanuk@gmail.com>
2022-04-01 16:06:53 +0200

    Add 13.6: Using Slugs in the Application

--- src/EntityListener/ConferenceEntityListener.php
+++ src/EntityListener/ConferenceEntityListener.php
@@ -1,12 +1,16 @@
+<?php
+	
 namespace App\EntityListener;
 
 use App\Entity\Conference;
-use Doctrine\ORM\Event\LifecyleEventArgs;
+use Doctrine\ORM\Event\LifecycleEventArgs;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ConferenceEntityListener
 {
-    private function __construct(SluggerInterface $slugger)
+    private $slugger;
+
+    public function __construct(SluggerInterface $slugger)
     {
         $this->slugger = $slugger;
     }
