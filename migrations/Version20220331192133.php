5ffdb65e9ef288cb6cfad35344e2f5d13b7b10f0
thecoderush <pheanuk@gmail.com>
2022-04-06 18:27:16 +0200

    Restoring migrations from The Fast Track Github Repository

--- migrations/Version20220331192133.php
+++ migrations/Version20220331192133.php
@@ -20,12 +20,13 @@ final class Version20220331192133 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_911533C8989D9B62 ON conference (slug)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX UNIQ_911533C8989D9B62');
     }
 }
