ea434b57715938cc11c77480ca84cdb42d1f83e6
thecoderush <pheanuk@gmail.com>
2022-04-13 14:17:10 +0200

    Add 26.1-2: Installing API Platform / Exposing an API for Conferences

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
