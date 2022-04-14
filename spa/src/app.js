0459438a717f2470af8e696e33829abd9fc8e699
thecoderush <pheanuk@gmail.com>
2022-04-14 18:21:05 +0200

    Add 27.6: Fetching Data from the API

--- spa/src/app.js
+++ spa/src/app.js
@@ -2,16 +2,27 @@ import '../assets/styles/app.scss';
 
 import {h, render} from 'preact';
 import {Router, Link} from 'preact-router';
+import {useState, useEffect} from 'preact/hooks';
 
+import {findConferences} from './api/api';
 import Home from './pages/home';
 import Conference from './pages/conference';
 
 function App() {
+    const [conferences, setConferences] = useState(null);
+
+    useEffect(() => {
+        findConferences().then((conferences) => setConferences(conferences));
+    }, []);
+
+    if (conferences === null) {
+        return <div className="text-center pt-5">Loading...</div>;
+    }
+
     return (
         <div>
-            Hello world!
             <header className="header">
-                <nav className="">
+                <nav className="navbar navbar-light bg-light">
                     <div className="container">
                         <Link className="navbar-brand mr-4 pr-2" href="/">
                             &#128217; Guestbook
@@ -21,14 +32,19 @@ function App() {
 
                 <nav className="bg-light border-bottom text-center">
                     <Link className="nav-conference" href="/conference/amsterdam2019">
-                        Amsterdam 2019
+                        amsterdam 2019
                     </Link>
+                    {conferences.map((conference) => (
+                        <Link className="nav-conference" href="{'/conference/'+conference.slug}">
+                            {conference.city} {conference.year}
+                        </Link>
+                    ))}
                 </nav>
            </header>
 
             <router>
-                <Home path="/" />
-                <Conference path="/conference/:slug" />
+                <Home path="/" conferences={conferences} />
+                <Conference path="/conference/:slug" conferences={conferences} />
             </router>
         </div>
     )
