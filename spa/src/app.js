f59ab93c96e4652059b5573c61378348c74b9036
thecoderush <pheanuk@gmail.com>
2022-04-14 10:50:28 +0200

    Add 27.4: Adding a Router to handle States

--- spa/src/app.js
+++ spa/src/app.js
@@ -1,9 +1,23 @@
 import {h, render} from 'preact';
+import {Router, Link} from 'preact-router';
+
+import Home from './pages/home';
+import Conference from './pages/conference';
 
 function App() {
     return (
         <div>
             Hello world!
+            <header>
+                <Link href="/">Home</Link>
+                <br />
+                <Link href="/conference/amsterdam2019">Amsterdam 2019</Link>
+           </header>
+
+            <router>
+                <Home path="/" />
+                <Conference path="/conference/:slug" />
+            </router>
         </div>
     )
 }
