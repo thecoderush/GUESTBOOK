00d9bc2de7dc4298a942131e23f0062e862f6016
thecoderush <pheanuk@gmail.com>
2022-04-14 15:06:36 +0200

    Add 27.5: Styling the spa

--- spa/src/app.js
+++ spa/src/app.js
@@ -1,3 +1,5 @@
+import '../assets/styles/app.scss';
+
 import {h, render} from 'preact';
 import {Router, Link} from 'preact-router';
 
@@ -8,10 +10,20 @@ function App() {
     return (
         <div>
             Hello world!
-            <header>
-                <Link href="/">Home</Link>
-                <br />
-                <Link href="/conference/amsterdam2019">Amsterdam 2019</Link>
+            <header className="header">
+                <nav className="">
+                    <div className="container">
+                        <Link className="navbar-brand mr-4 pr-2" href="/">
+                            &#128217; Guestbook
+                        </Link>
+                    </div>
+                </nav>
+
+                <nav className="bg-light border-bottom text-center">
+                    <Link className="nav-conference" href="/conference/amsterdam2019">
+                        Amsterdam 2019
+                    </Link>
+                </nav>
            </header>
 
             <router>
